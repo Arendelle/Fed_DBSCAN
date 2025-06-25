37e4670d5bd9894ec8272f334796f3939515b19a
Arendelle <arendelle@outlook.my>
2025-06-25 11:22:48 +0800

    feat: 绘制子图功能

--- out/drawplot.py
+++ out/drawplot.py
@@ -15,20 +15,20 @@ for ratio in mal_ratio:
 
 plt.title("Accuracy without Detection")
 plt.xlabel("Round")
-plt.ylabel("Accuracy")
-plt.legend()
+plt.ylabel("Accuracy (%)")
+plt.legend(title="Malicious Ratio")
 plt.grid()
 plt.show()
 
 # 绘制恶意客户端检测效果对比图
-mal_ratio = {'0.1', '0.2', '0.3', '0.4'}
-file_prefix_w = "output_acc_w_detection_mal_"
-file_prefix_wo = "output_acc_wo_detection_mal_"
+mal_ratio = ['0.1', '0.2', '0.3', '0.4']
+file_prefix_w = "output_acc_w_detection_mal_0."
+file_prefix_wo = "output_acc_wo_detection_mal_0."
 
-for ratio in mal_ratio:
-	plt.figure(figsize=(10, 5))
-
+plt.figure(figsize=(10, 8))
+for ratio in range(1, 5):
 	csv_reader = csv.reader(open(file_prefix_w + str(ratio) + '.csv'))
+	plt.subplot(220 + ratio)
 	for row in csv_reader:
 		float_row = [float(x) for x in row]
 		plt.plot(float_row, label='With Detection')
@@ -37,9 +37,11 @@ for ratio in mal_ratio:
 		float_row = [float(x) for x in row]
 		plt.plot(float_row, label='Without Detection')
 
-	plt.title("Accuracy, mal ratio = " + str(ratio))
+	plt.title("Accuracy, malicious ratio = " + str(ratio))
 	plt.xlabel("Round")
-	plt.ylabel("Accuracy")
+	plt.ylabel("Accuracy (%)")
 	plt.legend()
 	plt.grid()
-	plt.show()
+
+plt.tight_layout()
+plt.show()
